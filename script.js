7167abb5ab144c9986e9a8ccb48f730e4bd8c7e1
Phablynne <you@example.com>
2026-04-28 10:37:24 -0300

    calculadora javascript

---
+++ script.js
@@ -0,0 +1,39 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(btn => {
+    btn.addEventListener('click' , () => handleInput(btn.innerText));
+});
+
+function handleInput(value){
+    if (value === 'C'){
+        display.innertext = '0';
+} else if (value === '=') {
+    try{
+      display.innerText =  eval(display.innerText
+      .replace('x','*')
+      .replace('÷','/'));
+} catch {
+    display.innerText= 'Erro';
+  }
+} else {
+    if (display.innerText === '0'){
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+  }
+}
+
+//suporte ao teclado
+document.addEventListener('keydown',(e) => {
+    if(!insNan(e.key) || ['+', '-', '*', '/', '.'].includes(e.key)) {
+        hedleInput(e.key);
+    } else if (e.key === 'Enter') {
+    handleInput('=');
+    } else if (e.key === 'Backspace') {
+    display.innerText = display.innerText.slice(0, -1) || '0';
+    } else if (e.key.toLowerCase() === 'c') {
+    handleInput('C');
+    }
+});
